17443ce43fb4b76a2057f0932c03e09b933f00fa
Dan Nettleton <dnett@iastate.edu>
2019-04-12 09:02:51 -0500

    Add files via upload

--- S510/RepeatedMeasures.R
+++ S510/RepeatedMeasures.R
@@ -1,43 +1,45 @@
 #Read the data and create factors.
 
-d=read.delim("http://www.public.iastate.edu/~dnett/S511/RepeatedMeasures.txt")
+d=read.delim("http://dnett.github.io/S510/RepeatedMeasures.txt")
 head(d)
 
-d$Program=as.factor(d$Program)
-d$Subj=as.factor(d$Subj)
-d$Timef=as.factor(d$Time)
+d$Program = factor(d$Program)
+d$Subj = factor(d$Subj)
+d$Timef = factor(d$Time)
 head(d)
 
-# Compute sample means
+# Compute sample averages
 
-means = tapply(d$Strength,
+averages = tapply(d$Strength,
     list(d$Time,d$Program),mean)
-means
+averages
 
-#  Make a profile plot of the means
+# Make a profile plot of the averages
 
 x.axis = unique(d$Time)
 matplot(c(2,14), c(79,85.7), type="n", 
-     xlab="Time(Days)", ylab="Strength",
-     main= "Observed Strength Means")   
-matlines(x.axis,means,type='l',lty=c(1,2,3),lwd=2)
-matpoints(x.axis,means, pch=c(16,17,15),cex=1.5)    
-legend(2.1,85.69,legend=c("RI program",
-    'WI Program','Controls'),lty=c(1,2,3),
+     xlab="Time (Days)", ylab="Average Strength",
+     main= " ")   
+matlines(x.axis,averages,type='l',lty=c(1,2,3),lwd=2)
+matpoints(x.axis,averages, pch=c(16,17,15),cex=1.5)    
+legend(2.1,85.69,legend=c("Program 1 (Rep Increase)",
+    'Program 2 (Weight Increase','Program 3 (Control)'),lty=c(1,2,3),
     lwd=2,col=1:3,bty='n')
 
-# Use the lme function. This application
-# assumes that each subject has a different
-# identification value
+
+# Load the nlme package
 
 library(nlme)
-d.lme = lme(Strength ~ Program*Timef,
+
+# Use the lme function to fit the
+# linear mixed-effects model with
+# random subject effects.
+
+o.lme = lme(Strength ~ Program*Timef,
   random= ~ 1|Subj, data=d,
   method="REML")
-summary(d.lme)
-anova(d.lme)
-names(d.lme)
 
+summary(o.lme)
 
 #  Use the gls( ) function to fit a
 #  model where the errors have a 
@@ -45,58 +47,51 @@ names(d.lme)
 #  within subjects. Random effects are
 #  not used to induce correlation.
 
-d.glscs = gls(Strength ~ Program*Timef,
+o.cs = gls(Strength ~ Program*Timef,
   data=d,
   correlation = corCompSymm(form=~1|Subj),
   method="REML")
-summary(d.glscs)
-anova(d.glscs)
-
-
+summary(o.cs)
 
 # Try an auto regressive covariance 
 # structures across time within 
 # subjects
 
-d.glsar = gls(Strength ~ Program*Timef,
+o.ar1 = gls(Strength ~ Program*Timef,
   data=d,
   correlation = corAR1(form=~1|Subj),
   method="REML")
-summary(d.glsar)
-anova(d.glsar)
+summary(o.ar1)
 
-
-#  Use an arbitray covariance matrix for
-#  observations at different time 
+#  Use an unstructured covariance matrix
+#  for observations at different time 
 #  points within subjects
 
-d.gls = gls(Strength ~ Program*Timef,
+o.un = gls(Strength ~ Program*Timef,
   data=d,
   correlation = corSymm(form=~1|Subj),
   weight = varIdent(form = ~ 1|Timef),
   method="REML")
-summary(d.gls)
-anova(d.gls)
-
+summary(o.un)
 
 #  Compare the fit of various covariance
 #  structures.
 
-anova(d.gls, d.glscs)
-anova(d.gls, d.glsar)
+anova(o.cs, o.un)
+anova(o.ar1, o.un)
 
 
 # Treat time as a continuous variable and
 # fit quadratic trends in strength
 # over time
 
-d.time = gls(Strength ~ Program+Time+
+o.time = gls(Strength ~ Program+Time+
   Program*Time+I(Time^2)+Program*I(Time^2),
   data=d,
   correlation = corAR1(form=~1|Subj),
   method="REML")
-summary(d.time)
-anova(d.time)
+summary(o.time)
+anova(o.time)
 
 
 # To compare the continuous time model to the 
@@ -105,36 +100,36 @@ anova(d.time)
 # instead of REML likelihood values.
 
 
-d.glsarmle = gls(Strength ~ Program*Timef,
+o.ar1mle = gls(Strength ~ Program*Timef,
   data=d,
   correlation = corAR1(form=~1|Subj),
   method="ML")
 
 
-d.timemle = gls(Strength ~ Program+ Time+
+o.timemle = gls(Strength ~ Program+ Time+
      Program*Time+I(Time^2)+Program*I(Time^2),
   data=d,
   correlation = corAR1(form=~1|Subj),
   method="ML")
 
-anova(d.glsarmle, d.timemle)
+anova(o.ar1mle, o.timemle)
 
 
 #  Do not fit different quadratic trends
 #  for different programs
 
-d.timemle = gls(Strength ~ Program+Time+
+o.timemle = gls(Strength ~ Program+Time+
      Program*Time+I(Time^2), data=d,
   correlation = corAR1(form=~1|Subj),
   method="ML")
 
-anova(d.glsarmle, d.timemle)
+anova(o.ar1mle, o.timemle)
 
 
 #  Fit a model with random regression coefficients
 #  for individual subjects 
 
-d.timer = lme(Strength ~ Program+Time+
+o.timer = lme(Strength ~ Program+Time+
      Program*Time+I(Time^2), 
   random = ~ Time + I(Time^2) | Subj,
   data=d,
@@ -142,23 +137,23 @@ d.timer = lme(Strength ~ Program+Time+
   control=list(msMaxIter=100),
   method="REML")
 
-d.timer
+o.timer
 
-fixef(d.timer)
+fixef(o.timer)
 
-ranef(d.timer)
+ranef(o.timer)
 
-coef(d.timer)
+coef(o.timer)
 
 prog=d$Program[seq(7,nrow(d),by=7)]
 
-b=cbind(coef(d.timer)[,1]+
-       (prog==2)*coef(d.timer)[,2]+
-       (prog==3)*coef(d.timer)[,3],
-        coef(d.timer)[,4]+
-       (prog==2)*coef(d.timer)[,6]+
-       (prog==3)*coef(d.timer)[,7],
-        coef(d.timer)[,5])       
+b=cbind(coef(o.timer)[,1]+
+       (prog==2)*coef(o.timer)[,2]+
+       (prog==3)*coef(o.timer)[,3],
+        coef(o.timer)[,4]+
+       (prog==2)*coef(o.timer)[,6]+
+       (prog==3)*coef(o.timer)[,7],
+        coef(o.timer)[,5])       
 x=seq(2,14,by=.1)
 plot(c(2,14),c(71,90),ylim=c(71,90),pch=" ",
      ylab="Strength",xlab="Day")
@@ -171,13 +166,13 @@ legend(2.1,75,legend=c("RI program",
 # Do we need the AR(1) structure in the
 # random coefficients model?
 
-d.timeru = lme(Strength ~ Program+Time+
+o.timeru = lme(Strength ~ Program+Time+
      Program*Time+I(Time^2), 
   random = ~ Time + I(Time^2) | Subj,
   data=d,
   method="REML")
 
-anova(d.timer,d.timeru)
+anova(o.timer,o.timeru)
 
 # The more complicated model is preferred.
 # Keep the AR(1) structure.
